bdd078f7ffefaaabb80f1429f482f5cfb74b867e
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-04-15 14:50:50 -0700

    commit tree 36c454e6a7881aa758e23d644e180af89c3b3e61

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+
+function createEmployeeRecord(employeeArr) {
+let employeeRecord = {
+    firstName: employeeArr[0],
+    familyName: employeeArr[1],
+    title: employeeArr[2],
+    payPerHour: employeeArr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+    return employeeRecord
+}
+
+function createEmployeeRecords(arrOfEmployeeArrays){
+    let arraysOfEmployees = []
+    arraysOfEmployees.push(arrOfEmployeeArrays.forEach(createEmployeeRecord))
+    return arraysOfEmployees
+}
